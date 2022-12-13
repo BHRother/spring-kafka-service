f3a56a68eaf875803a936de6cc03839c6e937bb2
brunorother <BHRother@users.noreply.github.com>
2022-12-13 19:04:39 +0000

    Cleanup Database

--- src/test/java/br/com/itau/bootcamp/springkafkaservice/AbstractIntegrationTest.java
+++ src/test/java/br/com/itau/bootcamp/springkafkaservice/AbstractIntegrationTest.java
@@ -3,28 +3,46 @@ package br.com.itau.bootcamp.springkafkaservice;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.transaction.annotation.Transactional;
 import org.testcontainers.containers.KafkaContainer;
+import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.utility.DockerImageName;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 @ActiveProfiles(profiles = "test")
+@Sql(scripts={"classpath:cleanUpDatabase.sql"}, executionPhase = ExecutionPhase.AFTER_TEST_METHOD)
 public abstract class AbstractIntegrationTest {
 
     @Container
     public static KafkaContainer kafkaContainer = new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:6.2.1"));
+
+    @Container
+    public static PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>(DockerImageName.parse("postgres:13.4-buster"))
+        .withUsername("test")
+        .withPassword("test")
+        .withDatabaseName("kafkadbtest");
 
     @DynamicPropertySource
     static void kafkaProperties(DynamicPropertyRegistry registry) {
         if (!kafkaContainer.isRunning()) {
             kafkaContainer.start();
         }
+        if (!postgreSQLContainer.isRunning()) {
+            postgreSQLContainer.start();
+        }
         registry.add("spring.kafka.bootstrap-servers", kafkaContainer::getBootstrapServers);
+
+        System.setProperty("DB_URL", postgreSQLContainer.getJdbcUrl());
+        System.setProperty("DB_DRIVER_CLASS", postgreSQLContainer.getDriverClassName());
+        System.setProperty("DB_USERNAME", postgreSQLContainer.getUsername());
+        System.setProperty("DB_PASSWORD", postgreSQLContainer.getPassword());
     }
 }
